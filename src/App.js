acf20d9399a54d2093960340f0dc9607fccfc417
manavshah <manavshah1011.ms@gmail.com>
2022-08-20 13:02:15 +0530

    color changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
         <Helmet>
-                <style>{darkMode? `body {-webkit-appearance: none; background: linear-gradient(to left, #0f0c29, #1c1c31, #0f0c29);}`:`body { background: #757F9A;  /* fallback for old browsers */
+                <style>{darkMode? `body {-webkit-appearance: none; background: linear-gradient(to left, #070614, #0f0c29, #070614);}`:`body { background: #757F9A;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #D7DDE8, #757F9A);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #D7DDE8, #757F9A); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }`}</style>
